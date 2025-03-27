7f2082d6003b0bf77c7b0006cf7f13b701c5ab1b
vakosel <vakosel@yahoo.gr>
2025-03-27 22:15:51 +0200

    add term instead of ! when using F5 code runner in python

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<F5>", ":w <bar> exec '!python3 '.shellescape('%')<CR>")
+map("n", "<F5>", ":w <bar> exec 'term python '.shellescape('%')<CR>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
