dba448cdd3979687cb892afb868f5715ec7cb720
vakosel <vakosel@yahoo.gr>
2025-05-22 23:41:53 +0300

    radio thunar-wezterm scripts config mpv

--- lua/plugins/blink-tab.lua
+++ lua/plugins/blink-tab.lua
@@ -1,25 +1,27 @@
 return {
   "saghen/blink.cmp",
-  ---@class PluginLspOpts
   opts = {
     signature = { enabled = true },
     keymap = {
-      --     ["<CR>"] = { "accept", "fallback" },
       ["<Tab>"] = {
-        "select_next",
-        "snippet_forward",
-        "fallback",
+        "select_next", -- move to next item
+        "snippet_forward", -- or snippet jump
+        "fallback", -- or insert tab if nothing else
       },
       ["<S-Tab>"] = {
-        "select_prev",
-        "snippet_backward",
-        "fallback",
+        "select_prev", -- move to previous item
+        "snippet_backward", -- or jump back
+        "fallback", -- or do nothing
+      },
+      ["<CR>"] = {
+        "accept", -- confirm selected or first item
+        "fallback", -- if nothing, insert newline
       },
       ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
       ["<C-e>"] = { "hide" },
       ["<C-u>"] = { "scroll_documentation_up", "fallback" },
       ["<C-d>"] = { "scroll_documentation_down", "fallback" },
-      preset = "super-tab",
+      preset = "super-tab", -- keep this for Blink-style context
     },
   },
 }
