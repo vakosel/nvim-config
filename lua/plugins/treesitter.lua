0908c7c086afc9111696b308112e806c81d59ac7
vakosel <vakosel@yahoo.gr>
2025-10-12 21:05:58 +0300

    update nvim treesiter and mason files in plugins folder

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,4 @@
-return 
-
-{
+return {
   "nvim-treesitter/nvim-treesitter",
   version = false, -- last release is way too old and doesn't work on Windows
   build = ":TSUpdate",
@@ -69,7 +67,6 @@ return
   ---@param opts TSConfig
   config = function(_, opts)
     if type(opts.ensure_installed) == "table" then
-      ---@type table<string, boolean>
       local added = {}
       opts.ensure_installed = vim.tbl_filter(function(lang)
         if added[lang] then
@@ -79,6 +76,24 @@ return
         return true
       end, opts.ensure_installed)
     end
-    require("nvim-treesitter.configs").setup(opts)
+
+    local ok, ts = pcall(require, "nvim-treesitter.configs")
+    if ok then
+      ts.setup(opts)
+    end
   end,
+  --   config = function(_, opts)
+  --     if type(opts.ensure_installed) == "table" then
+  --       ---@type table<string, boolean>
+  --       local added = {}
+  --       opts.ensure_installed = vim.tbl_filter(function(lang)
+  --         if added[lang] then
+  --           return false
+  --         end
+  --         added[lang] = true
+  --         return true
+  --       end, opts.ensure_installed)
+  --     end
+  --     require("nvim-treesitter.configs").setup(opts)
+  --   end,
 }
