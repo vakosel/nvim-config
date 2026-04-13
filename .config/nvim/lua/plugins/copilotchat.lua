db64c4ab84c8163e04de15f687411c35da13d035
vakosel <vakosel@yahoo.gr>
2026-04-13 17:09:41 +0300

    copilot

---
+++ .config/nvim/lua/plugins/copilotchat.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    keys = {
+      { "<leader>ae", "<cmd>CopilotChatExplain<cr>", mode = "v", desc = "AI Explain" },
+      { "<leader>af", "<cmd>CopilotChatFix<cr>", mode = "v", desc = "AI Fix" },
+    },
+  },
+}
