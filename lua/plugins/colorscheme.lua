4dc2896130a583fb521f2810676a0d42a33ba564
vakosel <vakosel@yahoo.gr>
2026-04-19 23:20:11 +0300

    fish functions, juliana theme transparency fix

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -16,13 +16,45 @@ return {
     "kaiuri/nvim-juliana",
     lazy = false,
     priority = 1000,
-    -- We don't need a config function because the plugin is simple
     config = function()
-      -- Apply transparency manually since we aren't using a setup function
-      vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
+      vim.cmd.colorscheme("juliana")
 
-      vim.cmd("colorscheme juliana")
+      local function transparent()
+        local groups = {
+          "Normal",
+          "NormalNC",
+          "NormalFloat",
+          "FloatBorder",
+          "SignColumn",
+          "EndOfBuffer",
+          "VertSplit",
+        }
+
+        for _, group in ipairs(groups) do
+          vim.api.nvim_set_hl(0, group, { bg = "none" })
+        end
+      end
+
+      -- apply immediately
+      transparent()
+
+      -- re-apply every time a colorscheme is loaded (IMPORTANT)
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = transparent,
+      })
     end,
   },
+  -- {
+  --   "kaiuri/nvim-juliana",
+  --   lazy = false,
+  --   priority = 1000,
+  --   -- We don't need a config function because the plugin is simple
+  --   config = function()
+  --     -- Apply transparency manually since we aren't using a setup function
+  --     vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
+  --     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
+  --
+  --     vim.cmd("colorscheme juliana")
+  --   end,
+  -- },
 }
