9920abc717961be7050157fab98e0bd7bfe6cf3b
vakosel <vakosel@yahoo.gr>
2026-04-19 16:38:15 +0300

    Add adaptive AI config and UI eye candy

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -74,3 +74,8 @@ end
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/vakosel/.lmstudio/bin
+# End of LM Studio CLI section
+
