ba048a81245af5eb831872e76ae0fe02ba9df9de
vakosel <vakosel@yahoo.gr>
2026-04-21 00:57:21 +0300

    deleted nvim_backup copilotchat created pyproject.toml for micropython

--- lua/plugins/copilotchat.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    keys = {
-      { "<leader>ae", "<cmd>CopilotChatExplain<cr>", mode = "v", desc = "AI Explain" },
-      { "<leader>af", "<cmd>CopilotChatFix<cr>", mode = "v", desc = "AI Fix" },
-    },
-  },
-}
